9d4f0c80ab2c60c7bbf179215805350667a92740
iansw246 <ianjazz246@gmail.com>
2022-10-04 20:57:35 -0700

    Add GTests framework

---
+++ test/fuse-test.cpp
@@ -0,0 +1,10 @@
+#include "fuse.hpp"
+#include <gtest/gtest.h>
+
+// Demonstrate some basic assertions.
+TEST(FuseTest, GetAttr_Root) {
+  // Expect two strings not to be equal.
+  EXPECT_STRNE("hello", "world");
+  // Expect equality.
+  EXPECT_EQ(7 * 6, 42);
+}
